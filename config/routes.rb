c520c23e1b9deb6da60ac0f4d840bd48e8c53b68
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-15 16:29:32 -0500

    Controllers: Add recipes controller with proper views template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipes/show'
+  get 'recipes/new'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
