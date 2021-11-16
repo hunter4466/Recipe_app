833f712bd26c2b288929acf46821cfab5df2e76c
Sebastián M <smunozmo@gmail.com>
2021-11-15 21:21:46 -0500

    Add models and views for recipes.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,20 @@
+class RecipesController < ApplicationController
+  def new
+  end
+
+  def show
+    @user = current_user.id
+    @recipes = Recipe.all
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      # Post.update_post_counter(User.find(current_user.id))
+      flash[:notice] = 'Post deleted succesfully'
+    else
+      flash[:error] = 'Post not deleted'
+    end
+    redirect_to recipes_show_url()
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+         devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :inventories, foreign_key: 'user_id', class_name: 'Food'
   has_many :recipes, foreign_key: 'user_id', class_name: 'Recipe'
   has_many :foods, foreign_key: 'user_id', class_name: 'Food'
-
-  def self.most_recent_posts(user)
-    Post.joins(:author).where(author: { id: user.id }).order(created_at: :desc).limit(3)
-  end
 end
