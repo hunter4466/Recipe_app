6c6355b1792db58cf0aee8624ad8e266649c878b
Sebastián M <smunozmo@gmail.com>
2021-11-15 21:25:55 -0500

    Update recipes routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipes/show'
   get 'recipes/new'
+  get 'recipes/show', to:  "recipes#show", as: "recipes_show"
+  delete "/users/recipes/:id", to: "recipes#destroy", as: "user_destroy_recipe"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
