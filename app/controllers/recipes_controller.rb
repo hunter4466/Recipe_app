d4843b1e391b791b7816588af3ad5d1e2b7c9834
Sebastián M <smunozmo@gmail.com>
2021-11-16 08:25:03 -0500

    Add create recipe method.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,47 @@
 class RecipesController < ApplicationController
-  def new
-  end
 
   def show
     @user = current_user.id
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+    @user = current_user.id
+  end
+
+  def create
+    @recipe = Recipe.new
+    @recipe.name = params[:recipe][:name]
+    @recipe.preparation_time = params[:recipe][:preparation_time]
+    @recipe.cooking_time = params[:recipe][:cooking_time]
+    @recipe.description = params[:recipe][:description]
+    @recipe.user_id = params[:user_id]
+    @recipe.public = false
+    if @recipe.save
+      redirect_to recipes_show_url()
+    else
+      flash.now[:error] = 'To-do item update failed'
+      render :new
+    end
+  end
+
+  def public
+    @recipes = Recipe.all
+    @food = Food.all
+    # @foods = Foods.includes(:user).order(created_at: :desc)
+
+    # @posts = Post.joins(:author).where(author: { id: @user.id }).order(created_at: :desc)
+    # @comments = Comment.includes(:author).order(created_at: :desc)
+  end
+
   def destroy
     @recipe = Recipe.find(params[:id])
     if @recipe.destroy
       # Post.update_post_counter(User.find(current_user.id))
-      flash[:notice] = 'Recipe deleted succesfully'
+      flash[:notice] = 'Post deleted succesfully'
     else
-      flash[:error] = 'Recipe not deleted'
+      flash[:error] = 'Post not deleted'
     end
     redirect_to recipes_show_url()
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'foods/show', to: "foods#show", as: "foods_show"
   get 'foods/new', to: "foods#new", as: "foods_new"
-  get 'recipes/new'
+  get 'recipes/new', to: "recipes#new", as: "recipes_new"
   get 'recipes/show', to:  "recipes#show", as: "recipes_show"
   get 'recipes/public', to:  "recipes#public", as: "recipes_public"
   post "/users/:user_id/food", to: "foods#create", as: "user_create_food"
+  post "/users/:user_id/recipe", to: "recipes#create", as: "user_create_recipe"
   delete "/users/foods/:id", to: "foods#destroy", as: "user_destroy_food"
   delete "/users/recipes/:id", to: "recipes#destroy", as: "user_destroy_recipe"
   devise_for :users
